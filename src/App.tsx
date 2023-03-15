8a5a2e14b059141af7765b4e8b327f62865e0620
Vitali Bedik <vitalya.bedik@gmail.com>
2023-03-15 22:56:04 +0300

    finish 01-lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function hello() {
+  debugger
+  alert('Hello IT-KAMASUTRA')
+}
+
+// hello()
+
+
+// function declaration
 function App() {
+  debugger
+  // полезное что-то
+  // обязана вернуть JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        This APP component
+        <Rating/>
+        <Accourdion/>
+      </div>
   );
+}
+
+function Rating() {
+  debugger
+  return (
+      <div>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+      </div>
+  )
+}
+
+function Accourdion() {
+  debugger
+  return (
+      <div>
+        <h3>Меню</h3>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+  )
+}
+
+function Star() {
+  return (
+      <div>
+        <div>Star</div>
+      </div>
+  )
 }
 
 export default App;
