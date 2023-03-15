77f9930996bb1dd76b568c330d454390257be045
Vitali Bedik <vitalya.bedik@gmail.com>
2023-03-15 23:42:46 +0300

    finish 02-lesson

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 
 function hello() {
-  debugger
-  alert('Hello IT-KAMASUTRA')
+    debugger
+    alert('Hello IT-KAMASUTRA')
 }
 
 // hello()
@@ -11,51 +11,75 @@ function hello() {
 
 // function declaration
 function App() {
-  debugger
-  // полезное что-то
-  // обязана вернуть JSX
-  return (
-      <div className="App">
-        This APP component
-        <Rating/>
-        <Accourdion/>
-      </div>
-  );
+    console.log('App rendering')
+    // полезное что-то
+    // обязана вернуть JSX
+    return (
+        <div className="App">
+            <AppTitle/>
+            <Rating/>
+            <Accourdion/>
+            <Rating/>
+        </div>
+    );
+}
+
+function AppTitle() {
+    console.log('AppTitle rendering')
+    return <>This is APP component</>
 }
 
 function Rating() {
-  debugger
-  return (
-      <div>
-        <Star/>
-        <Star/>
-        <Star/>
-        <Star/>
-        <Star/>
-      </div>
-  )
+    console.log('Rating rendering')
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+
+    )
 }
 
 function Accourdion() {
-  debugger
-  return (
-      <div>
-        <h3>Меню</h3>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>
-      </div>
-  )
+    console.log('Accourdion rendering')
+    return (
+        <>
+            <AccourdionTitle/>
+            <AccourdionBody/>
+        </>
+    )
+}
+
+function AccourdionTitle() {
+    console.log('AccourdionTitle rendering')
+    return (
+        <>
+            <h3>Меню</h3>
+        </>
+    )
+}
+
+function AccourdionBody() {
+    console.log('AccourdionBody rendering')
+    return (
+        <>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </>
+    )
 }
 
 function Star() {
-  return (
-      <div>
+    console.log('Star rendering')
+    return (
         <div>Star</div>
-      </div>
-  )
+    )
 }
 
 export default App;
