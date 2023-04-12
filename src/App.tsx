cc47a0a8563a2fbc5c6413c2d140c330462507ef
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-12 23:25:16 +0300

    feat: 06-lesson with props

--- src/App.tsx
+++ src/App.tsx
@@ -19,19 +19,19 @@ function App() {
     // обязана вернуть JSX
     return (
         <div className="App">
-            <PageTitle title={'This is APP component'}/>
-            <PageTitle title={'My friends'}/>
-            Article 1
-            <Rating value={3}/>
-            <Accordion titleValue={'Menu'} collapsed={true}/>
-            <Accordion titleValue={'Users'} collapsed={false}/>
-            Article 2
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            {/*<PageTitle title={'This is APP component'}/>*/}
+            {/*<PageTitle title={'My friends'}/>*/}
+            {/*Article 1*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Accordion titleValue={'Menu'} collapsed={true}/>*/}
+            {/*<Accordion titleValue={'Users'} collapsed={false}/>*/}
+            {/*Article 2*/}
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
             <OnOff isOn/>
             <OnOff isOn={false}/>
         </div>
--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -4,14 +4,44 @@ type propsType = {
     isOn: boolean
 }
 
+
 export const OnOff = (props: propsType) => {
-    const className = props.isOn ? 'on' : 'off'
+
+
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '2px',
+        backgroundColor: props.isOn ? 'green' : 'white'
+    }
+
+    const offStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '5px',
+        padding: '2px',
+        backgroundColor: props.isOn ? 'white' : 'red'
+    }
+
+    const indicatorStyle = {
+        width: '10px',
+        height: '10px',
+        borderRadius: '5px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '5px',
+        backgroundColor: props.isOn ? 'green' : 'red'
+    }
 
     return (
         <div>
-            <button className={className}>on</button>
-            <button className={className}>off</button>
-            <span className={className}>o</span>
+            <div style={onStyle}>On</div>
+            <div style={offStyle}>Off</div>
+            <div style={indicatorStyle}></div>
         </div>
     )
 }
