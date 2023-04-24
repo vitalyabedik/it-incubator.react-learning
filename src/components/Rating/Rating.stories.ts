5afc4ee35ee5e2e1b360e9c4adbd69d7ef112a64
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-25 00:11:01 +0300

    feat: 10-lesson add storybook

---
+++ src/components/Rating/Rating.stories.ts
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { within, userEvent } from '@storybook/testing-library';
+
+import {Rating, RatingValueType} from './Rating';
+
+
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+    title: 'Rating',
+    component: Rating,
+    tags: ['myComponent'],
+    // argTypes: {
+    //     value: RatingValueType,
+    //     onClick: (value: RatingValueType) => void
+    // },
+} satisfies Meta<typeof Rating>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Star_1: Story = {
+    args: {
+        value: 1,
+    },
+};
+
+export const Star_2: Story = {
+    args: {
+        value: 2,
+    },
+};
+
+export const Star_3: Story = {
+    args: {
+        value: 3,
+    },
+};
+
+export const Star_4: Story = {
+    args: {
+        value: 4,
+    },
+};
+
+export const Star_5: Story = {
+    args: {
+        value: 5,
+    },
+};
+
+// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
+export const ChangeStar: Story = {
+    args: {
+        value: 2,
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+        const clickStar = await canvas.getByRole('button', {
+            name: '2',
+        });
+        await userEvent.click(clickStar);
+    },
+};
