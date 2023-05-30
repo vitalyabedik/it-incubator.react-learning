233bc7952a5a17743b658abfd37c7cd8829c478c
Vitali Bedik <vitalya.bedik@gmail.com>
2023-05-30 21:20:45 +0300

    feat: 21 - basics useEffect

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react';
+
+export default {
+    title: 'useEffect demo'
+}
+
+export const SimpleExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log('SimpleExample')
+
+    useEffect(() => {
+        console.log('useEffect every render')
+        document.title = counter.toString()
+    })
+
+    useEffect(() => {
+        console.log('useEffect only first render (componentDidMount)')
+        document.title = counter.toString()
+    }, [])
+
+    useEffect(() => {
+        console.log('useEffect first render and every counter change')
+        document.title = counter.toString()
+    }, [counter])
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>
+    </>
+}
