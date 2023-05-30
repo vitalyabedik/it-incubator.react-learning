8256d778fc117cb2987ba7526f637b44458ee13c
Vitali Bedik <vitalya.bedik@gmail.com>
2023-05-30 21:38:31 +0300

    feat: 22 - useEffect \ setTimeout \ setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -31,3 +31,29 @@ export const SimpleExample = () => {
         <button onClick={() => setCounter(counter + 1)}>counter+</button>
     </>
 }
+
+export const SetTimeoutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log('SetTimeoutExample')
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         console.log('setTimeout')
+    //         document.title = counter.toString()
+    //     }, 1000)
+    // }, [counter])
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter((prevCounter) => prevCounter + 1)
+        }, 1000)
+    }, [])
+
+    return <>
+        Hello, counter: {counter} - fake: {fake}
+        {/*<button onClick={() => setFake(fake + 1)}>fake+</button>*/}
+        {/*<button onClick={() => setCounter(counter + 1)}>counter+</button>*/}
+    </>
+}
